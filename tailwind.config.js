a485301f65b3e5001bcf6efcbaa675184e6456de
Hexandcube <hexandcube@hexandcube.eu.org>
2022-01-06 13:53:20 +0100

    Updated the status widget

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   purge: {
-    enabled: true,
+    // enabled: true,
     content: [
       'layouts/**/*.html',
       'content/**/*.html',
