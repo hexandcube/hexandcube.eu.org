46a6f824bc7014d8751c78252cd45735cf48a622
Hexandcube <hexandcube@hexandcube.eu.org>
2024-04-02 19:45:13 +0200

    Add new UI Color Tokens to Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,47 @@
-// Tailwind shades - Step up:7, Step down: 4
-
 module.exports = {
   content: ["layouts/**/*.{html,md}", "content/**/*.{html,md}"],
   darkMode: "class",
   theme: {
     extend: {
+      opacity: {
+        2: "0.02",
+        4: "0.04",
+        6: "0.06",
+        8: "0.08",
+        10: "0.10",
+        12: "0.12",
+        14: "0.14",
+        16: "0.16",
+        20: "0.20",
+        24: "0.24",
+        26: "0.26",
+        28: "0.28",
+        32: "0.32",
+        28: "0.36",
+        42: "0.42",
+        44: "0.44",
+        48: "0.48",
+        54: "0.54",
+        56: "0.56",
+        60: "0.60",
+        64: "0.64",
+        72: "0.72",
+        80: "0.80",
+        88: "0.88",
+      },
       colors: {
-        // Legacy colors
-        light: {
-          "primary-text": "#fefefe",
-          "secondary-text": "#1a1a1a",
-          text: "#1c1917",
-          "primary-bg": "#f5f5f4",
-          "secondary-bg": "#efeeed",
-          selection: "#e0e0dd",
-          border: "#d3d1cf",
-          grey: "#666666",
-        },
-        dark: {
-          "primary-text": "#fefefe",
-          "secondary-text": "#e5ddda",
-          text: "#e7e1e1",
-          "primary-bg": "#0f121a",
-          "secondary-bg": "#191c24",
-          selection: "#292d38",
-          border: "#21242d",
-          grey: "#a8adbd",
-        },
-
         // Color Pallete
         white: "rgb(255, 255, 255)",
         black: "rgb(0, 0, 0)",
-        gray: {
-          "gray-100": "rgb(239, 239, 239)",
-          "gray-200": "rgb(223, 223, 223)",
-          "gray-300": "rgb(207, 207, 207)",
-          "gray-400": "rgb(175, 175, 175)",
-          "gray-500": "rgb(143, 143, 143)",
-          "gray-600": "rgb(112, 112, 112)",
-          "gray-700": "rgb(80, 80, 80)",
-          "gray-800": "rgb(48, 48, 48)",
+        grey: {
+          100: "rgb(239, 239, 239)",
+          200: "rgb(223, 223, 223)",
+          300: "rgb(207, 207, 207)",
+          400: "rgb(175, 175, 175)",
+          500: "rgb(143, 143, 143)",
+          600: "rgb(112, 112, 112)",
+          700: "rgb(80, 80, 80)",
+          800: "rgb(48, 48, 48)",
         },
         orange: {
           DEFAULT: "rgb(255, 0, 0)",
@@ -104,7 +106,7 @@ module.exports = {
           400: "rgb(255, 143, 143)",
           500: "rgb(255, 80, 80)",
           600: "rgb(215, 40, 40)",
-          700: "hsl(0, 88%, 33%)",
+          700: "rgb(158, 10, 10)",
           800: "rgb(96, 0, 0)",
         },
         deepblue: {
@@ -118,8 +120,9 @@ module.exports = {
           700: "rgb(13, 72, 191)",
           800: "rgb(0, 40, 120)",
         },
+        // LEGACY PLACEHOLDER COLORS - TODO: REMOVE AFTER MIGRATION
         accent: {
-          DEFAULT: "rgb(253, 100, 70)",
+          DEFAULT: "rgb(255, 0, 0)",
           50: "rgb(255, 0, 0)",
           100: "rgb(255, 0, 0)",
           200: "rgb(255, 0, 0)",
@@ -134,7 +137,165 @@ module.exports = {
 
         // UI Tokens
         current: "currentColor",
-        transparent: "transparent"
+        transparent: "transparent",
+        light: {
+          brand: {
+            primary: "rgb(253, 100, 70)",
+            secondary: "rgb(239, 90, 60)", // orange-500
+            tertiary: "rgb(252, 145, 124)",
+            muted: "rgb(255, 195, 183)",
+          },
+          text: {
+            primary: "rgb(0, 0, 0)", // black
+            secondary: "rgb(110, 110, 110)", // black 0.57 on l2 bg
+            disabled: "rgb(168, 168, 168)", // black 0.34 on l2 bg
+            inverse: "rgb(255, 255, 255)", // white
+            link: "rgb(239, 90, 60)", // accent-orange-primary
+          },
+          cta: {
+            primary: {
+              DEFAULT: "rgb(0, 0, 0)", // black
+              hover: "rgb(41, 41, 41)",
+            },
+            secondary: {
+              DEFAULT: "rgb(255, 255, 255)", // white
+              hover: "rgb(245, 245, 245)",
+            },
+            disabled: "rgb(224, 224, 224)", // black 0.12 on l2 bg
+            chip: {
+              hover: "rgb(240, 240, 240)", // black 0.06 on l2 bg
+            },
+          },
+          border: {
+            primary: "rgb(224, 224, 224)", // black 0.12 on l2 bg
+            secondary: "rgb(235, 235, 235)", // black 0.08 on l2 bg
+            tertiary: "rgb(245, 245, 245)", // black 0.04 on l2 bg
+          },
+          bg: {
+            l0: "rgb(245, 245, 245)",
+            l1: "rgb(250, 250, 250)",
+            l2: "rgb(255, 255, 255)", // white
+            l3: "rgb(255, 255, 255)", // white
+          },
+          accent: {
+            orange: {
+              primary: "rgb(239, 90, 60)", // orange-500
+              secondary: "rgb(255, 214, 206)", // orange-400 0.36 on l2 bg
+            },
+            green: {
+              primary: "rgb(0, 160, 94)", // green-500
+              secondary: "rgb(214, 237, 220)", // green-400 0.28 on l2 bg
+            },
+            pink: {
+              primary: "rgb(175, 88, 131)", // pink-600
+              secondary: "rgb(249, 217, 233)", // pink-400 0.36 on l2 bg
+            },
+            yellow: {
+              primary: "rgb(183, 137, 0)", // yellow-500
+              secondary: "rgb(240, 215, 139)", // yellow-400 0.48 on l2 bg
+            },
+            blue: {
+              primary: "rgb(11, 121, 175)", // blue-600
+              secondary: "rgb(190, 229, 249)", // blue-400 0.36 on l2 bg
+            },
+            red: {
+              primary: "rgb(215, 40, 40)", // red-600
+              secondary: "rgb(255, 215, 215)", // red-400 0.36 on l2 bg
+            },
+            deepblue: {
+              primary: "rgb(13, 72, 191)", // deepblue-700
+              secondary: "rgb(212, 226, 255)", // deepblue-400 0.36 on l2 bg
+            },
+          },
+          // LEGACY PLACEHOLDER COLORS - TODO: REMOVE AFTER MIGRATION
+          "primary-text": "#ff0000",
+          "secondary-text": "#ff0000",
+          text: "#ff0000",
+          "primary-bg": "#ff0000",
+          "secondary-bg": "#ff0000",
+          selection: "#ff0000",
+          border: "#ff0000",
+          grey: "#ff0000",
+        },
+        dark: {
+          brand: {
+            primary: "rgb(253, 100, 70)",
+            secondary: "rgb(255, 142, 120)", // orange-400
+            tertiary: "rgb(94, 62, 56)",
+            muted: "rgb(54, 42, 40)",
+          },
+          text: {
+            primary: "rgb(255, 255, 255)", // white
+            secondary: "rgb(165, 165, 165)", // white 0.60 on l2 bg
+            disabled: "rgb(91, 91, 91)", // white 0.27 on l2 bg
+            inverse: "rgb(0, 0, 0)", // black
+            link: "rgb(226, 128, 108)", // accent-orange-primary
+          },
+          cta: {
+            primary: {
+              DEFAULT: "rgb(255, 255, 255)", // white
+              hover: "rgb(235, 235, 235)",
+            },
+            secondary: {
+              DEFAULT: "rgb(255, 255, 255)", // white
+              hover: "rgb(245, 245, 245)",
+            },
+            disabled: "rgb(58, 58, 58)", // white 0.12 on l2 bg
+            chip: {
+              hover: "rgb(49, 49, 49)", // white 0.08 on l2 bg
+            },
+          },
+          border: {
+            primary: "rgb(58, 58, 58)", // black 0.12 on l2 bg
+            secondary: "rgb(49, 49, 49)", // black 0.08 on l2 bg
+            tertiary: "rgb(40, 40, 40)", // black 0.04 on l2 bg
+          },
+          bg: {
+            l0: "rgb(22, 22, 22)",
+            l1: "rgb(26, 26, 26)",
+            l2: "rgb(31, 31, 31)",
+            l3: "rgb(36, 36, 36)",
+          },
+          accent: {
+            orange: {
+              primary: "rgb(226, 128, 108)", // orange-400 0.87 on l2 bg
+              secondary: "rgb(112, 71, 63)", // orange-400 0.36 on l2 bg
+            },
+            green: {
+              primary: "rgb(26, 177, 115)", // green-400 0.87 on l2 bg
+              secondary: "rgb(29, 78, 58)", // green-400 0.32 on l2 bg
+            },
+            pink: {
+              primary: "rgb(212, 134, 173)", // pink-400 0.87 on l2 bg
+              secondary: "rgb(106, 74, 90)", // pink-400 0.36 on l2 bg
+            },
+            yellow: {
+              primary: "rgb(198, 153, 16)", // yellow-400 0.87 on l2 bg
+              secondary: "rgb(100, 81, 25)", // yellow-400 0.36 on l2 bg
+            },
+            blue: {
+              primary: "rgb(69, 163, 211)", // blue-400 0.87 on l2 bg
+              secondary: "rgb(47, 86, 105)", // blue-400 0.36 on l2 bg
+            },
+            red: {
+              primary: "rgb(226, 129, 129)", // red-400 0.87 on l2 bg
+              secondary: "rgb(112, 71, 71)", // red-400 0.36 on l2 bg
+            },
+            deepblue: {
+              primary: "rgb(121, 156, 226)", // deepblue-400 0.87 on l2 bg
+              secondary: "rgb(68, 83, 112)", // deepblue-400 0.36 on l2 bg
+            },
+          },
+          // LEGACY PLACEHOLDER COLORS - TODO: REMOVE AFTER MIGRATION
+          "primary-text": "#ff0000",
+          "secondary-text": "#ff0000",
+          text: "#ff0000",
+          "primary-bg": "#ff0000",
+          "secondary-bg": "#ff0000",
+          selection: "#ff0000",
+          border: "#ff0000",
+          grey: "#ff0000",
+        },
       },
       screens: {
         "3xl": "1750px",
